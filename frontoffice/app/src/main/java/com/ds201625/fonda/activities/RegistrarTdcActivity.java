7a1ea9b777f949bf4b896fdc3bf89bc2a8039b99
melaniegodoy <melanie.g1257@gmail.com>
2016-04-13 18:02:51 -0430

    FO_M9_PagoOrden
    Comienzo de la parte de pago de orden y prueba de boton

---
+++ frontoffice/app/src/main/java/com/ds201625/fonda/activities/RegistrarTdcActivity.java
@@ -0,0 +1,15 @@
+package com.ds201625.fonda.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ds201625.fonda.R;
+
+public class RegistrarTdcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_tdc);
+    }
+}
