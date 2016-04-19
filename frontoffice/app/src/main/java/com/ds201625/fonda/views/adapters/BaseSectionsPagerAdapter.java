38f6d885015d8ae0336d5e10282c9981e875de2d
Rómulo Rodríguez <rodriguezrjrr@gmail.com>
2016-04-18 23:23:09 -0430

    FO - ToolBar con tabs + Iconos en titulo.

--- frontoffice/app/src/main/java/com/ds201625/fonda/views/adapters/BaseSectionsPagerAdapter.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/views/adapters/BaseSectionsPagerAdapter.java
@@ -1,8 +1,15 @@
 package com.ds201625.fonda.views.adapters;
 
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+
+import com.ds201625.fonda.R;
 
 import java.util.ArrayList;
 
@@ -22,15 +29,28 @@ public class BaseSectionsPagerAdapter extends FragmentPagerAdapter {
      */
     private ArrayList<String> titles;
 
+    private ArrayList<Drawable> icons;
+    private TabLayout tabLayout;
+
     /**
      * Constructor
      * @param fm
      */
+    public BaseSectionsPagerAdapter(FragmentManager fm, TabLayout tabLayout) {
+        super(fm);
+        fragments = new ArrayList<Fragment>();
+        titles = new ArrayList<String>();
+        icons = new ArrayList<Drawable>();
+        this.tabLayout = tabLayout;
+    }
+
     public BaseSectionsPagerAdapter(FragmentManager fm) {
         super(fm);
         fragments = new ArrayList<Fragment>();
         titles = new ArrayList<String>();
-    }
+        icons = new ArrayList<Drawable>();
+        tabLayout = null;
+     }
 
     /**
      * Agrega un Fragment
@@ -40,7 +60,36 @@ public class BaseSectionsPagerAdapter extends FragmentPagerAdapter {
     public void addFragment(String title, Fragment fm) {
         fragments.add(fm);
         titles.add(title);
+        icons.add(null);
         notifyDataSetChanged();
+    }
+
+    public void addFragment(Drawable titleIcon, Fragment fm) {
+        if (tabLayout == null)
+            return;
+        fragments.add(fm);
+        titles.add("");
+        icons.add(titleIcon);
+        notifyDataSetChanged();
+    }
+
+    public void addFragment(String title, Drawable titleIcon, Fragment fm) {
+        if (tabLayout == null)
+            return;
+        fragments.add(fm);
+        titles.add(title);
+        icons.add(titleIcon);
+        notifyDataSetChanged();
+    }
+
+    public void iconsSetup() {
+        notifyDataSetChanged();
+        for(int i = 0; i<fragments.size() ; i++) {
+            Drawable icon = icons.get(i);
+            if (icon != null) {
+                tabLayout.getTabAt(i).setIcon(icon);
+            }
+        }
     }
 
     @Override
