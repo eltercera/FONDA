e10bfb8d627ca3b32897c2e144a93abdae0803c2
Jesus Trejo <jesustrejo10@gmail.com>
2016-04-20 20:56:30 -0430

    pantallcreacion de la actividad detailRestaurantActivty, (pantalla de detalle del restaurante)

---
+++ frontoffice/app/src/main/java/com/ds201625/fonda/views/activities/DetailRestaurantActivity.java
@@ -0,0 +1,19 @@
+package com.ds201625.fonda.views.activities;
+
+import android.os.Bundle;
+
+import com.ds201625.fonda.R;
+
+/**
+ * Created by jesus on 20/04/16.
+ */
+public class DetailRestaurantActivity extends BaseNavigationActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_detail_restaurant);
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
