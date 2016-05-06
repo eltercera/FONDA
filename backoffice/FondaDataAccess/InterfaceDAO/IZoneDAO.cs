2033497ea6836bc62bc0e9a4592b38a823e0e826
Marco Franceschi <marcofranceschi@hotmail.com>
2016-05-06 14:04:13 -0400

    Agregando interfaces del Data Access

---
+++ backoffice/FondaDataAccess/InterfaceDAO/IZoneDAO.cs
@@ -0,0 +1,13 @@
+﻿using com.ds201625.fonda.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ds201625.fonda.DataAccess.InterfaceDAO
+{
+    public interface IZoneDAO : IBaseEntityDAO<Zone>
+    {
+    }
+}
