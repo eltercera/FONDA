e6e11db3e9ad8268625e5a8727b3924485a9a047
MalenaLi <malenarojasy@gmail.com>
2016-05-07 14:19:37 -0400

    Hibernate Restaurant, Category, Table

---
+++ backoffice/FondaDataAccess/HibernateDAO/HibernateTableDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.ds201625.fonda.Domain;
+using com.ds201625.fonda.DataAccess.InterfaceDAO;
+
+namespace com.ds201625.fonda.DataAccess.HibernateDAO
+{
+    public class HibernateTableDAO : HibernateBaseEntityDAO<Table> , ITableDAO
+    {
+    }
+}
---
+++ backoffice/FondaLogicLayer/Implementation/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using com.ds201625.fonda.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ds201625.fonda.LogicLayer.Implementation
+{
+    public class Restaurant : com.ds201625.fonda.Domain.Restaurant
+    {
+
+    public Restaurant()
+    {
+
+    }
+
+    }
+}
