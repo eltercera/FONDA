ad3fbcc6543ea0fd75eabad6fe0a6bd36615120e
chispa1990 <alexanderperez1991@yahoo.es>
2016-05-07 10:42:07 -0430

    acomodando prueba dish LISTO
    se agrego la prueba dish de data access

--- backoffice/FondaDataAccessTest/BODishTest.cs
+++ backoffice/FondaDataAccessTest/BODishTest.cs
@@ -4,113 +4,99 @@ using com.ds201625.fonda.DataAccess.FactoryDAO;
 using com.ds201625.fonda.DataAccess.InterfaceDAO;
 using com.ds201625.fonda.Domain;
 
-namespace com.ds201625.fonda.Tests.DataAccess
+namespace DataAccess
 {
-    /* [TestFixture()]
 
-       public class BODishTest
-       {
+    [TestFixture]
 
+    public class BODishTest
+    {
+        private FactoryDAO _facDAO;
+        private IDishDAO _dishDAO;
+        private Dish _dish;
+        private int _dishId;
 
-         private FactoryDAO _facDAO;
+        /// <summary>
+        /// Prueba de Dominio.
+        /// crea un plato.
+        /// </summary>
+        [Test]
+        public void DishTest()
+        {
+            generateDish();
+            dishAssertions();
 
-         private IDishDAO _dishDAO;
-         private Dish _dish;
-         private int _dishId;
+        }
 
-         /// <summary>
-         /// Prueba de Dominio.
-         /// crea un plato.
-         /// </summary>
-         [Test()]
-         public void DishSave()
-         {
-             generateDish();
-             dishAssertions();
+        [Test]
 
-               _dishDAO.Save(_dish);
+        public void DishSave()
+        {
+            getDishDao();
+            generateDish();
 
-               Assert.AreNotEqual(_dish.Id, 0);
-               _dishId = _dish.Id;
+            _dishDAO.Save(_dish);
 
-               generateDish(true);
+            Assert.AreNotEqual(_dish.Id, 0);
+            _dishId = _dish.Id;
 
-               _dishDAO.Save(_dish);
+            generateDish(true);
 
-               _dishDAO.ResetSession();
+            _dishDAO.Save(_dish);
 
-               _dish = null;
+            _dishDAO.ResetSession();
 
-               _dish = _dishDAO.FindById(_dishId);
-
-               dishAssertions(true);
-
-         }
-
-           private void getDishDao()
-           {
-               getDao();
-               if (_dishDAO == null)
-                   _dishDAO = _facDAO.GetDishDAO();
-
-           }
-
-           private void getDao()
-           {
-               if (_facDAO == null)
-                   _facDAO = FactoryDAO.Intance;
-           }
+            _dish = null;
 
 
+        }
 
+        private void getDishDao()
+        {
+            getDao();
+            if (_dishDAO == null)
+                _dishDAO = _facDAO.GetDishDAO();
 
-           private void generateDish(bool edit = false)
-         {
-             if (_dish != null & !edit)
-                 return;
+        }
 
-             if ((edit & _dish == null) | _dish == null)
-                 _dish = new Dish();
-
-             string editadd = "";
-
-             if (edit)
-                 editadd = "Editado";
-
-             _dish.Name = "Rol de Canela" + editadd;
-             _dish.Description = "Da diarrea" + editadd;
-             _dish.Cost = 580;
-             _dish.Image = null;
-             _dish.Status =  ActiveSimpleStatus.Instance;
-             _dish.Suggestion = true;
+        private void getDao()
+        {
+            if (_facDAO == null)
+                _facDAO = FactoryDAO.Intance;
+        }
 
 
 
-         }
+        private void generateDish(bool edit = false)
+        {
+            if (_dish != null)
+                return;
 
-         private void dishAssertions(bool edit = false)
-         {
+            if ((edit & _dish == null) | _dish == null)
+                _dish = new Dish();
 
-             Assert.IsNotNull(_dish);
-             Assert.AreEqual(_dish.Name, "Rol de Canela");
-             Assert.AreEqual(_dish.Description, "Da diarrea");
-             Assert.AreEqual( _dish.Cost,580);
-             Assert.AreEqual(_dish.Image, null);
-             Assert.AreEqual(_dish.Status, ActiveSimpleStatus.Instance);
-             Assert.AreEqual(_dish.Suggestion ,true);
-         }
+            _dish.Name = "Pasta";
+            _dish.Description = "con carne";
+            _dish.Cost = 195;
+            _dish.Image = null;
+            _dish.Suggestion = true;
+            _dish.Status = ActiveSimpleStatus.Instance;
 
-           [TestFixtureTearDown]
-           public void EndTests()
-           {
-               if (_dishId != 0)
-               {
-                   getDishDao();
-                   // Eliminacion del plato al finalidar todo.
-                   //_dishDAO.Delete(_dish);
-               }
-               _dishDAO.ResetSession();
-           }
+        }
 
-       }*/
+
+
+        private void dishAssertions(bool edit = false)
+        {
+
+            Assert.IsNotNull(_dish);
+            Assert.AreEqual(_dish.Name, "Pasta");
+            Assert.AreEqual(_dish.Description, "con carne");
+            Assert.AreEqual(_dish.Cost, 195);
+            Assert.AreEqual(_dish.Image, null);
+            Assert.AreEqual(_dish.Status, ActiveSimpleStatus.Instance);
+            Assert.AreEqual(_dish.Suggestion, true);
+        }
+
+    }
 }
