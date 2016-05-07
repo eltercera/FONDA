379183bb8a03ace78e6d17adbc9648e42ccccd0a
eilincastillo <eilinalejandra@hotmail.com>
2016-05-07 14:13:19 -0400

    Modificación en clase coordinate, Creación de RestaurantCategoryMap, RestaurantMap y TableMap

---
+++ backoffice/FondaDataAccess/HibernateDAO/FluentMappings/RestaurantCategoryMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace com.ds201625.fonda.DataAccess.HibernateDAO.FluentMappings
+{
+    class RestaurantCategoryMap : ClassMap<com.ds201625.fonda.Domain.RestaurantCategory>
+    {
+        public RestaurantCategoryMap()
+        {
+            Table("RESTAURANTCATEGORY");
+
+            Id(x => x.Id)
+              .Column("res_cat_id")
+              .Not.Nullable()
+              .GeneratedBy.Increment();
+
+            Map(x => x.Name)
+              .Column("fk_res_cat_name")
+              .Not.Nullable();
+
+        }
+    }
+}
---
+++ backoffice/FondaDataAccess/HibernateDAO/FluentMappings/RestaurantMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ds201625.fonda.DataAccess.HibernateDAO.FluentMappings
+{
+    class RestaurantMap
+    {
+    }
+}
