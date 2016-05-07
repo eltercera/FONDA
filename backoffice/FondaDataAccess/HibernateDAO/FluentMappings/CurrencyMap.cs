c5a8061751ade6af6e6656db063e2163ff8675c2
eilincastillo <eilinalejandra@hotmail.com>
2016-05-07 17:43:00 -0400

    Creación de CurrencyMap y DayMap

---
+++ backoffice/FondaDataAccess/HibernateDAO/FluentMappings/CurrencyMap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace com.ds201625.fonda.DataAccess.HibernateDAO.FluentMappings
+{
+    class CurrencyMap : ClassMap<com.ds201625.fonda.Domain.Currency>
+    {
+        public CurrencyMap()
+        {
+            Table("CURRENCY");
+
+            Id(x => x.Id)
+              .Column("cu_id")
+              .Not.Nullable()
+              .GeneratedBy.Increment();
+
+            Map(x => x.Name)
+              .Column("cur_name")
+              .Not.Nullable();
+
+            Map(x => x.Symbol)
+                .Column("cur_symbol")
+                .Not.Nullable();
+        }
+
+    }
+}
---
+++ backoffice/FondaDataAccess/HibernateDAO/FluentMappings/DayMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace com.ds201625.fonda.DataAccess.HibernateDAO.FluentMappings
+{
+    class DayMap : ClassMap<com.ds201625.fonda.Domain.MenuCategory>
+    {
+        public DayMap()
+        {
+            Table("DAY");
+
+            Id(x => x.Id)
+              .Column("day_id")
+              .Not.Nullable()
+              .GeneratedBy.Increment();
+
+            Map(x => x.Name)
+              .Column("day_name")
+              .Not.Nullable();
+
+        }
+    }
+}
