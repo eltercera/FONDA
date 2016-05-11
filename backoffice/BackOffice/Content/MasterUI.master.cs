76fc89c01fbe3882a9ca8733953db03d459b9b92
josefg19 <josefg19@gmail.com>
2016-05-11 12:04:39 -0400

    Modificacion_master_page
    se  realizo la carga de menu por rol del usuario esta desabilitado  por
    los momentos.

--- backoffice/BackOffice/Content/MasterUI.master.cs
+++ backoffice/BackOffice/Content/MasterUI.master.cs
@@ -27,11 +27,32 @@ public partial class MasterUI : System.Web.UI.MasterPage
     }
     protected void Page_Load(object sender, EventArgs e)
     {
-        DropDownMenu();
+
+        /*  try
+            {
+            ///verifica si un usuario esta en el sistema
+       if (Session[RecursoMaster.sessionUserID] != null)
+                {*/
+        Session[RecursoMaster.sessionRol] = "Sistema";// rol del usuario log solo para control de prueba
+                    DropDownMenu();
+               /* }
+         else   
+             Response.Redirect(RecursoMaster.addressLogin);
+         }
+     catch (NullReferenceException ex)
+     {
+         //Response.Redirect(RecursoMaster.addressLogin);
+     }
+     catch (Exception ex)
+     {
+
+     } */
     }
 
     protected void DropDownMenu()
     {
+        string rol = (string)(Session[RecursoMaster.sessionRol]);
+        string[] permisos;//se guardaran los permisos asociados a cada opcion del menu.xml
         XmlDocument doc = new XmlDocument();
         string icon;//se guardaran para cargarel icono del menu
         int i = 0; //iteracion de posicionOpciones
@@ -39,6 +60,9 @@ public partial class MasterUI : System.Web.UI.MasterPage
         foreach (XmlNode node in doc.DocumentElement.ChildNodes)
         {
             i = 0;
+            permisos = node.Attributes[sessionTag.rolTag].InnerText.Split(char.Parse(RecursoMaster.splitRoles));
+               if (validateRol(permisos, rol))
+                {
             icon = node.Attributes[sessionTag.imgTag].Value;
             string[,] positionOpcinon = new string[2, node.ChildNodes.Count];
             foreach (XmlNode subNode in node.ChildNodes)
@@ -53,10 +77,19 @@ public partial class MasterUI : System.Web.UI.MasterPage
             subpositionOpcinonMenu[node.Attributes[sessionTag.nameTag].InnerText + ":" + node.Attributes[sessionTag.imgTag].InnerText] = positionOpcinon;
         }
 
+        }
     }
 
-
-
+    protected Boolean validateRol(string[] permisos, string rolUser)
+        {
+            foreach (string rol in permisos)
+            {
+                if (rol == rolUser)
+                    return true;
+            }
+            return false;
+        }
+    
 
 }
         
