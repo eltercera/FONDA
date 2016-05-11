b1b9bdcbe80ac9a7239e0f60c3787a02e3c61286
chispa1990 <alexanderperez1991@yahoo.es>
2016-05-11 19:20:33 -0430

    lleno tabla de prueba con lista
    lleno tabla de prueba GridView1 con objetos categoria menu

--- backoffice/BackOffice/Seccion/Menu/ListarCategoria.aspx
+++ backoffice/BackOffice/Seccion/Menu/ListarCategoria.aspx
@@ -152,6 +152,18 @@
                         </div>
                     </div>
                 </div>
+                <div class="row">
+
+                    <div>  
+                        <asp:GridView ID="GridView1" HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White"  
+                            runat="server" AutoGenerateColumns="false">  
+                            <Columns>  
+                                <asp:BoundField DataField="Id" HeaderText="Id" ItemStyle-Width="30" />  
+                                <asp:BoundField DataField="Name" HeaderText="Nombre" ItemStyle-Width="150" />    
+                            </Columns>  
+                        </asp:GridView>  
+                    </div>  
+
                 </div>
                 <!-- /.row -->
             <!-- /.container-fluid -->
--- backoffice/BackOffice/Seccion/Menu/ListarCategoria.aspx.cs
+++ backoffice/BackOffice/Seccion/Menu/ListarCategoria.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using com.ds201625.fonda.DataAccess.InterfaceDAO;
 using com.ds201625.fonda.DataAccess.FactoryDAO;
 using com.ds201625.fonda.Domain;
+using System.Data;
 
 namespace BackOffice.Seccion.Menu
 {
@@ -16,6 +17,28 @@ namespace BackOffice.Seccion.Menu
         {
             AlertSuccess_AgregarCategoria.Visible = false;
             AlertSuccess_ModificarCategoria.Visible = false;
+             
+            if (!this.IsPostBack)  
+            {
+                //Aqui se deneran los objetos para hacer consulta y generar la lista de categorias
+                FactoryDAO factoryDAO = FactoryDAO.Intance;
+                IMenuCategoryDAO _mencatDAO = factoryDAO.GetMenuCategoryDAO();
+                MenuCategory _mencat = _mencatDAO.FindById(1);
+                IList<MenuCategory> obj = _mencatDAO.GetAll();
+
+                DataTable dt = new DataTable();  
+                dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Id", typeof(int)),  new DataColumn("Name", typeof(string))});
+                int longitud = obj.Count;
+                
+                //se recorre el objeto lista y se muestra en la tabla 
+                for (int i=0; i<=longitud-1; i++)
+                {
+                    dt.Rows.Add(obj[i].Id, obj[i].Name);  
+                }
+                
+                GridView1.DataSource = dt;  
+                GridView1.DataBind();  
+            } 
         }
 
         protected void BotonAgregarCategoria_Click(object sender, EventArgs e)
