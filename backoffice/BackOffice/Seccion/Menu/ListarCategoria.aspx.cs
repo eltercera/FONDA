92e4125ffd85031a884efb6e2e6429bfa090cddc
Andreinabetlo <Andreinabetlo@gmail.com>
2016-05-15 16:58:03 -0400

    arreglando la carga de las tablas

--- backoffice/BackOffice/Seccion/Menu/ListarCategoria.aspx.cs
+++ backoffice/BackOffice/Seccion/Menu/ListarCategoria.aspx.cs
@@ -20,56 +20,32 @@ namespace BackOffice.Seccion.Menu
         }
 
 
+       
+
         protected void Page_Load(object sender, EventArgs e)
-        {   //Aqui atrapo el valor que me mandaria el restaurante en este caso el id
-            //int nombreparametro= int.Parse(Request.QueryString["parametrouno"]);
+        {   
             AlertSuccess_AgregarCategoria.Visible = false;
             AlertSuccess_ModificarCategoria.Visible = false;
              
             if (!this.IsPostBack)  
             {
-                //Aqui se generan los objetos para hacer consulta y generar la lista de categorias
-                FactoryDAO factoryDAO = FactoryDAO.Intance;
-                IMenuCategoryDAO _mencatDAO = factoryDAO.GetMenuCategoryDAO();
-                MenuCategory _mencat = _mencatDAO.FindById(1);
-                IList<MenuCategory> obj = _mencatDAO.GetAll();
-
-        
-                int longitud = obj.Count;
-                
-                //se recorre el objeto lista y se muestra en la tabla 
-                for (int i=0; i<=longitud-1; i++)
-                {
-                   //aqui se carga y se arma la tabla html recursomenu es un archivo donde hay cadenas de string muy grandes
-                    mencat += "<tr><td>" + obj[i].Name + "</td>";
-
-                    if (obj[i].Status.ToString() == "Activo")
-                    {
-                        mencat += RecursosMenu1.Activo;
-                    }
-                    else
-                    {
-                        mencat += RecursosMenu1.Inactivo;
-                    }
-
-                    mencat += RecursosMenu1.Acciones;
-                    mencat += RecursosMenu1.CerrarTR;
-
-                }
+                IList<MenuCategory> obj = Generar_Lista();
+                Llenar_Tabla(obj);
             } 
         }
 
         protected void BotonAgregarCategoria_Click(object sender, EventArgs e)
         {
             AlertSuccess_AgregarCategoria.Visible = true;
-            FactoryDAO factoryDAO = FactoryDAO.Intance;
-            IMenuCategoryDAO _mencatDAO = factoryDAO.GetMenuCategoryDAO();
+
             MenuCategory _mencat = new MenuCategory();
             String nombre = Value1.Text;
             _mencat.Name = nombre;
             _mencat.ListDish = null;
             _mencat.Status = ActiveSimpleStatus.Instance;
-            _mencatDAO.Save(_mencat);
+            Guardar_Categoria(_mencat);
+            IList<MenuCategory> obj = Generar_Lista();
+            Llenar_Tabla(obj);
         }
 
         protected void BotonModificarCategoria_Click(object sender, EventArgs e)
@@ -82,13 +58,57 @@ namespace BackOffice.Seccion.Menu
             String nombre1 = TextBox1.Text;
             _menu.Name = nombre1;
             _menu.Status = ActiveSimpleStatus.Instance;
-            _menucatDAO.Save(_menu);
-
-
-           
-           
+            _menucatDAO.Save(_menu);           
         }
 
-    
-    }
+    //metodo donde se llena la tabla desde una lista de objetos
+        public void Llenar_Tabla(IList<MenuCategory> obj)
+        {
+            int longitud = obj.Count;
+            mencat = null;
+            //se recorre el objeto lista y se muestra en la tabla 
+            for (int i = 0; i <= longitud - 1; i++)
+            {
+                //aqui se carga y se arma la tabla html recursomenu es un archivo donde hay cadenas de string muy grandes
+                mencat += "<tr><td>" + obj[i].Name + "</td>";
+
+                if (obj[i].Status.ToString() == "Activo")
+                {
+                    mencat += RecursosMenu1.Activo;
+                }
+                else
+                {
+                    mencat += RecursosMenu1.Inactivo;
+                }
+
+                mencat += RecursosMenu1.Acciones;
+                mencat += RecursosMenu1.CerrarTR;
+
+
+            }
+       
+     }
+
+    //Genera una lista de tipo Menu Categoria
+     public IList<MenuCategory> Generar_Lista()
+     {
+         FactoryDAO factoryDAO = FactoryDAO.Intance;
+         IMenuCategoryDAO _mencatDAO = factoryDAO.GetMenuCategoryDAO();
+         IList<MenuCategory> obj = _mencatDAO.GetAll();
+         return obj;
+     }
+
+     //guarda objeto menu categoria en la base de datos
+     public void Guardar_Categoria(MenuCategory obj)
+     {
+
+         FactoryDAO factoryDAO = FactoryDAO.Intance;
+         IMenuCategoryDAO _mencatDAO = factoryDAO.GetMenuCategoryDAO();
+         _mencatDAO.Save(obj);
+
+
+     }
+
+
+  }  
 }
