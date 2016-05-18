1c9d167a76774eb66b443b8aadeb564db436e625
adrianabetta <adrianabetta@gmail.com>
2016-05-18 12:58:41 -0430

    FO_M5_DaTosDinamicosConWS
    Se trabaja toda la lista expandible con objetos
    Se modifico la clase Expandable List Adapter
    Se modifico la clase del fragment history Vistits
    Se agregaron comentarios a los metodos

--- frontoffice/app/src/main/java/com/ds201625/fonda/views/adapters/ExpandableListAdapter.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/views/adapters/ExpandableListAdapter.java
@@ -14,38 +14,63 @@ import com.ds201625.fonda.R;
 import com.ds201625.fonda.domains.Invoice;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Adri on 13/04/2016.
  */
+
+/**
+ *  Clase para llenar la lista expandible de el historial de visitas de restaurant
+ */
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
     private Context context;
     private Map<String, List<String>> collectionVisits;
-    private List<String> visits;
-    private String[] date;
-    private String[] location;
-    private String[] shortDescription;
+    private List<String> nameRestaurant;
+    private List<String> categoryRestaurant;
+    private List<String> addressRestaurant;
+    private List<String> datePaymentRestaurant;
 
-
-
-    public ExpandableListAdapter(Context context, List<String> visits,
-                                 Map<String, List<String>> collectionVisits, String[] shortDescription, String[] location, String[] date) {
+    /**
+     *Constructor de la clase ExpandableListAdapter
+     * @param context
+     * @param nameRestaurant
+     * @param collectionVisits
+     * @param categoryRestaurant
+     * @param addressRestaurant
+     * @param datePaymentRestaurant
+     */
+    public ExpandableListAdapter(Context context, List<String> nameRestaurant,
+                                 Map<String, List<String>> collectionVisits, List<String> categoryRestaurant, List<String> addressRestaurant, List<String> datePaymentRestaurant) {
         this.context = context;
         this.collectionVisits = collectionVisits;
-        this.date = date;
-        this.visits = visits;
-        this.location = location;
-        this.shortDescription = shortDescription;
-
+        this.nameRestaurant = nameRestaurant;
+        this.datePaymentRestaurant = datePaymentRestaurant;
+        this.categoryRestaurant = categoryRestaurant;
+        this.addressRestaurant = addressRestaurant;
     }
 
+    /**
+     * Metodo para obtener el hijo de la lista expandible
+     * @param groupPosition
+     * @param childPosition
+     * @return Lista de String con el nombre del Restaurant
+     */
     public Object getChild(int groupPosition, int childPosition) {
-        return collectionVisits.get(visits.get(groupPosition)).get(childPosition);
+        return collectionVisits.get(nameRestaurant.get(groupPosition)).get(childPosition);
     }
 
-
+    /**
+     * Metodo que pinta la lista expandible de la vista de los hijos
+     * @param groupPosition
+     * @param childPosition
+     * @param isLastChild
+     * @param convertView
+     * @param parent
+     * @return la vista del ConverView de los hijos
+     */
     public View getChildView(final int groupPosition, final int childPosition,
                              boolean isLastChild, View convertView, ViewGroup parent) {
         final String detail = (String) getChild(groupPosition, childPosition);
@@ -54,15 +79,25 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.child_item_detail_visit, null);
         }
-
         TextView item = (TextView) convertView.findViewById(R.id.detail_restaurant);
         item.setText(detail);
         return convertView;
     }
 
+    /**
+     * Metodo que pinta la lista expandible de los padres
+     * @param groupPosition
+     * @param isExpanded
+     * @param convertView
+     * @param parent
+     * @return la vista  del converView del grupo
+     */
     public View getGroupView(int groupPosition, boolean isExpanded,
                              View convertView, ViewGroup parent) {
         String restaurantList = (String) getGroup(groupPosition);
+        String addressList = (String) getGroupAddress(groupPosition);
+        String categoryList = (String) getGroupCategory(groupPosition);
+        String datePaymentList =(String) getGroupDate(groupPosition);
 
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) context
@@ -86,43 +121,98 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             icon.setImageResource(R.mipmap.ic_restaurant004);
         if((restaurantList).equals("Tony's"))
             icon.setImageResource(R.mipmap.ic_restaurant005);
-
         nameRestaurant.setTypeface(null, Typeface.BOLD);
         nameRestaurant.setText(restaurantList);
-     //   nameRestaurant.setText(name[groupPosition]);
-        locationRestaurant.setText(location[groupPosition]);
-        descriptionRestaurant.setText(shortDescription[groupPosition]);
-        dateVisit.setText(date[groupPosition]);
+        locationRestaurant.setText(addressList);
+        descriptionRestaurant.setText(categoryList);
+        dateVisit.setText(datePaymentList);
         return convertView;
     }
 
-
+    /**
+     * metodo qe obtiene el numero de hijos de la lista expandible
+     * @param groupPosition
+     * @return entero con la cantidad de hijos de la lista.
+     */
     public int getChildrenCount(int groupPosition) {
-        return collectionVisits.get(visits.get(groupPosition)).size();
+        return collectionVisits.get(nameRestaurant.get(groupPosition)).size();
     }
 
+    /**
+     * metodo que obtiene el grupo la lista expandible
+     * @param groupPosition
+     * @return nombre de restaurant
+     */
     public Object getGroup(int groupPosition) {
-        return visits.get(groupPosition);
+        return nameRestaurant.get(groupPosition);
     }
 
+    /**
+     * metodo que obtiene el grupo la lista expandible
+     * @param groupPosition
+     * @return direccion de Restaurant
+     */
+    public Object getGroupAddress(int groupPosition) {
+        return addressRestaurant.get(groupPosition);
+    }
 
+    /**
+     * metodo que obtiene el grupo la lista expandible
+     * @param groupPosition
+     * @return categoria de Restaurant
+     */
+    public Object getGroupCategory(int groupPosition) {
+        return categoryRestaurant.get(groupPosition);
+    }
+
+    /**
+     * metodo que obtiene el grupo la lista expandible
+     * @param groupPosition
+     * @return fecha de Restaurant
+     */
+    public Object getGroupDate(int groupPosition) {
+        return datePaymentRestaurant.get(groupPosition);
+    }
+
+    /**
+     * metodo que obtiene la cantidad de elementos del grupo padre de la lista expandible
+     * @return numero de elementos
+     */
     public int getGroupCount() {
-        return visits.size();
+        return nameRestaurant.size();
     }
 
+    /**
+     * Metodo que obtiene el id del grupo padre de la lista expadible
+     * @param groupPosition
+     * @return variable long que indica el id del grupo
+     */
     public long getGroupId(int groupPosition) {
         return groupPosition;
     }
 
+    /**
+     * Metodo que obtiene el id del grupo de hijos de la lista expadible
+     * @param groupPosition
+     * @param childPosition
+     * @return variable long que indica el id del grupo
+     */
     public long getChildId(int groupPosition, int childPosition) {
         return childPosition;
     }
 
-
+    /**
+     * Metodo que verica el estado de los id de la lista expandible
+     * @return variable boolean
+     */
     public boolean hasStableIds() {
         return true;
     }
 
+    /**
+     * Metodo que verifica la seleccion de los hijos de la lista expandible
+     * @return variable boolean
+     */
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
     }
--- frontoffice/app/src/main/java/com/ds201625/fonda/views/fragments/HistoryVisitFragment.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/views/fragments/HistoryVisitFragment.java
@@ -1,6 +1,5 @@
 package com.ds201625.fonda.views.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,16 +10,8 @@ import android.widget.Toast;
 import com.ds201625.fonda.R;
 import com.ds201625.fonda.data_access.factory.FondaServiceFactory;
 import com.ds201625.fonda.data_access.services.HistoryVisitsRestaurantService;
-import com.ds201625.fonda.data_access.services.ProfileService;
 import com.ds201625.fonda.domains.Invoice;
-import com.ds201625.fonda.domains.Profile;
-import com.ds201625.fonda.domains.Restaurant;
-import com.ds201625.fonda.domains.RestaurantCategory;
 import com.ds201625.fonda.views.adapters.ExpandableListAdapter;
-
-import java.lang.reflect.Array;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -30,96 +21,31 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Clase de Prueba para mostar el uso de Fragments
+ * Clase de Fragment que maneja el Historial de Visitas al Restaurant
  */
 public class HistoryVisitFragment extends BaseFragment {
-    List<String> groupList;
-    List<String> childList;
-    Map<String, List<String>> collectionVisits;
-    ExpandableListView expListView;
-    private List<Invoice> p;
+    private List<String> groupNameRestaurant;
+    private List<String> groupCategoryRestaurant;
+    private List<String> groupAddressRestaurant;
+    private List<String> groupDatePaymentRestaurant;
+    private List<String> childList;
+    private Map<String, List<String>> collectionVisits;
+    private ExpandableListView expListView;
+    private HistoryVisitsRestaurantService histoyVisitsRestaurantService;
+    private List<Invoice> listInvoice;
 
-
-    //ATRIBUTOS DE PRUEBA
-
-    ArrayList<RestaurantCategory> listCategory = new ArrayList<>();
-    ArrayList<Restaurant> listRestaurant = new ArrayList<>();
-    ArrayList<Invoice> listInvoice = new ArrayList<>();
-    String  date = "12/10/2015";
-    Date datepayment;
-
-    // SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-    //categorias prueba
-    RestaurantCategory category1 = new RestaurantCategory("Romantico");
-    RestaurantCategory category2 = new RestaurantCategory("Casual");
-    RestaurantCategory category3 = new RestaurantCategory("Italiano");
-    RestaurantCategory category4 = new RestaurantCategory("Americano");
-    //restaurantes prueba
-    Restaurant restaurant1 = new Restaurant("The dining room", "La Castellana", category2);
-    Restaurant restaurant2 = new Restaurant("Mogi Mirin", "Los Dos Caminos", category1);
-    Restaurant restaurant3 = new Restaurant("Gordo & Magro", "La California", category3);
-    Restaurant restaurant4 = new Restaurant("La Casona", "Parque Central", category3);
-    Restaurant restaurant5 = new Restaurant("Tony's", "El Rosal", category4);
-
-    Profile profile = new Profile(1, "Adriana");
-
-    //facturas prueba
-    Invoice  invoice1 = new Invoice(200,600,800,restaurant1,datepayment, profile);
-    Invoice  invoice2 = new Invoice(300,600,800,restaurant2,datepayment, profile);
-    Invoice  invoice3 = new Invoice(200,600,800,restaurant3,datepayment, profile);
-    Invoice  invoice4 = new Invoice(200,600,800,restaurant4,datepayment, profile);
-    Invoice  invoice5 = new Invoice(200,600,800,restaurant3,datepayment, profile);
-  //  Invoice  invoice6 = new Invoice(200,600,800,restaurant3,datepayment, profile);
-   // Invoice  invoice7 = new Invoice(200,600,800,restaurant4,datepayment, profile);
-   // Invoice  invoice8 = new Invoice(200,600,800,restaurant4,datepayment, profile);
-    //Invoice  invoice9 = new Invoice(200,600,800,restaurant5,datepayment, profile);
-    //Invoice  invoice10 = new Invoice(200,600,800,restaurant5,datepayment,profile);
-
-    //FIN ATRIBUTOS DE PRUEBA
-
+    /**
+     * Metodo que se ejecuta al instanciar el Fragment
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       //lista categoria prueba
-        listCategory.add(category1);
-        listCategory.add(category2);
-        listCategory.add(category3);
-        listCategory.add(category4);
-
-        //lista restaurant prueba
-        listRestaurant.add(restaurant1);
-        listRestaurant.add(restaurant2);
-        listRestaurant.add(restaurant3);
-        listRestaurant.add(restaurant4);
-        listRestaurant.add(restaurant5);
-
-    /*    int idPerson = 1;
-        HistoryVisitsRestaurantService ps = FondaServiceFactory.getInstance().getHistoryVisitsService();
-        p=ps.getHistoryVisits();
-        for (int i = 0; i< p.size(); i ++){
-            System.out.println("mamalooo  "+p.get(i).getTax());
-            String newstring = new SimpleDateFormat("yyyy-MM-dd").format(p.get(i).getDate());
-            System.out.println("mamalooo1  "+newstring);
-        }*/
-
-
-
-
-        //lista factura prueba
-        listInvoice.add(invoice1);
-        listInvoice.add(invoice2);
-        listInvoice.add(invoice3);
-        listInvoice.add(invoice4);
-        listInvoice.add(invoice5);
-      //  listInvoice.add(invoice6);
-       // listInvoice.add(invoice7);
-        //listInvoice.add(invoice8);
-        //listInvoice.add(invoice9);
-        //listInvoice.add(invoice10);
-
     }
 
+    /**
+     * Metodo que crea la vista del historial de visitas o pagos al restaurant
+     */
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -129,12 +55,21 @@ public class HistoryVisitFragment extends BaseFragment {
 
         expListView = (ExpandableListView) layout.findViewById(R.id.restaurant_list);
         final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
-                getContext(), groupList,collectionVisits, shortDescription,location, dates);
+                getContext(),  groupNameRestaurant,collectionVisits,  groupCategoryRestaurant,
+                groupAddressRestaurant, groupDatePaymentRestaurant);
         expListView.setAdapter(expListAdapter);
-
 
         expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
 
+            /** Metodo para cargar la vista del detalle de historial de pagos
+             * Metodo
+             * @param parent
+             * @param v
+             * @param groupPosition
+             * @param childPosition
+             * @param id
+             * @return variable boolean
+             */
             public boolean onChildClick(ExpandableListView parent, View v,
                                         int groupPosition, int childPosition, long id) {
                 final String selected = (String) expListAdapter.getChild(
@@ -146,62 +81,47 @@ public class HistoryVisitFragment extends BaseFragment {
             }
         });
         return layout;
-
     }
 
-    private ArrayList<Invoice> pruebaMetodo(int idPersona) {
+    /**
+     * Metodo que consulta el historial de pagos en el Web Service
+     */
+    private void createGroupList() {
+        try{
+            histoyVisitsRestaurantService = FondaServiceFactory.getInstance().getHistoryVisitsService();
+            listInvoice= histoyVisitsRestaurantService.getHistoryVisits();
+        }catch (Exception e){
+            System.out.println("asd");
+        }
+        catch (ExceptionInInitializerError e){
 
-        ArrayList<Invoice> lista = new ArrayList<>();
-
+        }
+        groupNameRestaurant = new ArrayList<String>();
+        groupAddressRestaurant= new ArrayList<String>();
+        groupCategoryRestaurant= new ArrayList<String>();
+        groupDatePaymentRestaurant = new ArrayList<String>();
         Iterator iterator = listInvoice.listIterator();
         while (iterator.hasNext()) {
             Invoice invoice = (Invoice) iterator.next();
-            int idProfile = invoice.getProfile().getId();
-            if (idProfile == idPersona) {
-                lista.add(invoice);
-            }
-        }
-        return lista;
-    }
-
-     String[] dates ={
-            "12/01/16",
-            "12/01/16",
-            "12/01/16",
-            "12/01/16",
-            "12/01/16"};
-
-    String[] location = {
-            "La castellana",
-            "Los dos caminos",
-            "La California",
-            "Parque central",
-            "El Rosal"} ;
-    String[] shortDescription = {
-            "Casual",
-            "Romantico",
-            "Italiano",
-            "Italiano",
-            "Americano"} ;
-
-
-    private void createGroupList() {
-
-       groupList = new ArrayList<String>();
-        Iterator iterator = pruebaMetodo(1).listIterator();
-        while (iterator.hasNext()) {
-            Invoice invoice = (Invoice) iterator.next();
             String nameRestaurant = invoice.getRestaurant().getName();
-            groupList.add(nameRestaurant);
+            String addresRestaurant = invoice.getRestaurant().getAddress();
+            String categoryRestaurant = invoice.getRestaurant().getRestaurantCategory().getNameCategory();
+            Date datePayment = invoice.getDate();
+            String newstring = new SimpleDateFormat("dd-MM-yyyy").format(datePayment);
+            groupNameRestaurant.add(nameRestaurant);
+            groupAddressRestaurant.add(addresRestaurant);
+            groupCategoryRestaurant.add(categoryRestaurant);
+            groupDatePaymentRestaurant.add(newstring );
         }
         }
 
+    /**
+     * Metodo que maneja el detalle del historial del pago del Restaurant
+     */
     private void createCollection() {
         // preparing detailRestaurant for collection(child)
         collectionVisits = new LinkedHashMap<String, List<String>>();
-        //HistoryVisitsRestaurantService ps = FondaServiceFactory.getInstance().getHistoryVisitsService();
-        //p=ps.getHistoryVisits();
-/*        Iterator iterator = p.listIterator();
+        Iterator iterator = listInvoice.listIterator();
         while (iterator.hasNext()) {
             Invoice invoice = (Invoice) iterator.next();
             String nameRestaurant = invoice.getRestaurant().getName();
@@ -210,37 +130,28 @@ public class HistoryVisitFragment extends BaseFragment {
             float tax = invoice.getTax();
             float tip= invoice.getTip();
             float totalPayment = invoice.getTotal();
-            String datePayment = new SimpleDateFormat("yyyy-MM-dd").format(invoice.getDate());
+            String datePayment = new SimpleDateFormat("dd-MM-yyyy").format(invoice.getDate());
             String name= invoice.getProfile().getProfileName();
             String[] data1 = {"Nombre: "+name, "Restaurant :"+nameRestaurant,"Direccion: "
                     +addresRestaurant,"Categoria: "+categoryRestaurant,"Fecha: "+datePayment,"Propina: "
                     +tip,"I.V.A: "+tax,"Monto Cancelado: "+totalPayment};
 
-            for (String listName : groupList) {
+            for (String listName : groupNameRestaurant) {
                 if (listName.equals(nameRestaurant)) {
                     loadChild(data1);
                     collectionVisits.put(listName, childList);
                 }
             }
         }
-*/
     }
 
+    /**
+     * Metodo que carga a una lista el detalle del historial de pagos del restaurant
+     * @param restaurantDetails
+     */
     private void loadChild(String[] restaurantDetails) {
         childList = new ArrayList<String>();
         for (String model : restaurantDetails)
             childList.add(model);
     }
-
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
 }
