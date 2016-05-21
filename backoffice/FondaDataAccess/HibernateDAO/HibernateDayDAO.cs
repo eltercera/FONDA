4f1a97e61804ef40228a40f7c730f241cd2b9eb3
Marco Franceschi <marcofranceschi@hotmail.com>
2016-05-20 23:16:06 -0400

    Solucion a duplicacion

--- backoffice/FondaDataAccess/HibernateDAO/HibernateDayDAO.cs
+++ backoffice/FondaDataAccess/HibernateDAO/HibernateDayDAO.cs
@@ -5,5 +5,42 @@ namespace com.ds201625.fonda.DataAccess.HibernateDAO
 {
     public class HibernateDayDAO : HibernateNounBaseEntityDAO<Day> , IDayDAO
     {
+        /// <summary>
+        /// Devuelve el dia a partir del nombre
+        /// </summary>
+        /// <param name="name">Nombre del dia</param>
+        /// <returns>Objeto de tipo Dia</returns>
+        public Day GetDay(string name)
+        {
+            Day day = new Day();
+            switch (name)
+            {
+                case "Lunes":
+                    day = FindById(1);
+                    break;
+                case "Martes":
+                    day = FindById(2);
+                    break;
+                case "Miercoles":
+                    day = FindById(3);
+                    break;
+                case "Jueves":
+                    day = FindById(4);
+                    break;
+                case "Viernes":
+                    day = FindById(5);
+                    break;
+                case "Sabado":
+                    day = FindById(6);
+                    break;
+                case "Domingo":
+                    day = FindById(7);
+                    break;
+                default:
+                    break;
+            }
+
+            return day;
+        }
     }
 }
--- backoffice/FondaDataAccess/HibernateDAO/HibernateRestaurantCategoryDAO.cs
+++ backoffice/FondaDataAccess/HibernateDAO/HibernateRestaurantCategoryDAO.cs
@@ -9,9 +9,25 @@ namespace com.ds201625.fonda.DataAccess.HibernateDAO
 {
     public class HibernateRestaurantCategoryDAO : HibernateNounBaseEntityDAO<RestaurantCategory>, IRestaurantCategoryDAO
     {
+        /// <summary>
+        /// Devuelve la lista de todas las Categorias por Restaurante de la Base de Datos
+        /// </summary>
+        /// <returns>Lista de tipo RestaurantCategory</returns>
         public IList<RestaurantCategory> GetAll()
         {
             return FindAll();
+        }
+
+        /// <summary>
+        /// Devuelve una categoria de la Base de Datos
+        /// </summary>
+        /// <param name="name">Nombre de la categoria</param>
+        /// <returns>Objeto tipo RestaurantCategory</returns>
+        public RestaurantCategory GetRestaurantCategory(string name)
+        {
+            RestaurantCategory category = new RestaurantCategory();
+            category = FindBy("rc_name", name);
+            return category;
         }
 
     }
