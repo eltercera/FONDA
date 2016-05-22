b2bb934b9f93f14d05a460f1a98e2d5ab587ee98
adrianabetta <adri1991@hotmail.com>
2016-05-22 18:26:15 -0430

    Modificacion WS

--- frontoffice/app/src/main/java/com/ds201625/fonda/data_access/retrofit_client/clients/RetrofitService.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/data_access/retrofit_client/clients/RetrofitService.java
@@ -37,7 +37,7 @@ public class RetrofitService {
      * todo Ver como colocar esto usando el /etc/hosts del telefono.
      */
 
-    private final String API_BASE_URL = "http://fonda.eltercera.com.ve:8080/api/";
+    private final String API_BASE_URL = "http://192.168.1.105:5300/api/";
     /**
      * Cliente http
      */
