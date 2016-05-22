f21e379b2ef904e62595f99e885557fb938c9b6e
adrianabetta <adri1991@hotmail.com>
2016-05-22 12:35:44 -0430

    M5_PostPayment

--- frontoffice/app/src/main/java/com/ds201625/fonda/data_access/services/PaymentService.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/data_access/services/PaymentService.java
@@ -1,27 +1,19 @@
 package com.ds201625.fonda.data_access.services;
 
+import android.app.backup.RestoreObserver;
 import android.content.Context;
 
 import com.ds201625.fonda.data_access.local_storage.LocalStorageException;
+import com.ds201625.fonda.data_access.retrofit_client.InvalidDataRetrofitException;
 import com.ds201625.fonda.data_access.retrofit_client.RestClientException;
 import com.ds201625.fonda.domains.CreditCarPayment;
+import com.ds201625.fonda.domains.Invoice;
 
 /**
  * Created by Hp on 22/05/2016.
  */
 public interface PaymentService {
 
-    /**
-     *
-     * @param idProfile
-     * @param tip
-     * @param digits
-     * @param context
-     * @return
-     * @throws RestClientException
-     * @throws LocalStorageException
-     */
-    CreditCarPayment registerPayment(int idProfile, float tip, String digits, Context context) throws RestClientException, LocalStorageException;
-
+    Invoice setPayments(Invoice invoice) throws RestClientException, InvalidDataRetrofitException;
 
     }
