37dada6c5edaaae1505a0615b142cff3c7f9edaa
jessikad <jessikadaboin1@gmail.com>
2016-05-21 22:39:11 -0430

    g1

--- backoffice/BackOffice/Login.aspx.cs
+++ backoffice/BackOffice/Login.aspx.cs
@@ -18,8 +18,6 @@ namespace BackOffice.Seccion.Configuracion
   
         protected void Page_Load(object sender, EventArgs e)
         {
-
-
             errorLogin.Visible = false;
             warningLog.Visible = false;
 
@@ -33,14 +31,14 @@ namespace BackOffice.Seccion.Configuracion
             else
                 successLog.Visible = false;
         }
+
         public void Uservalidate(object sender, EventArgs e)
         {
-
             Uservalidate();
         }
+
         public void Recoverpassword(object sender, EventArgs e)
         {
-
             Recoverpassword();
         }
 
@@ -76,7 +74,6 @@ namespace BackOffice.Seccion.Configuracion
                     successLog.InnerText = message; break;
             }
         }
-
        
    /// <summary>
    /// Metodo que valida el intento de ingreso al sistema
@@ -135,6 +132,7 @@ namespace BackOffice.Seccion.Configuracion
             }
 
         }
+
        /// <summary>
         /// Metodo encargado de validar los campos de resstablecer contraseña 
        /// </summary>
@@ -149,12 +147,8 @@ namespace BackOffice.Seccion.Configuracion
 
             if (email != "" & passwordnew1 != "" & passwordnew2 != "" & username != "")
             {
-               
-
                 if (_employee != null)
                 {
-
-
                     if (_employee.UserAccount != null)
                     {
                         if (email.Equals(_employee.UserAccount.Email))
@@ -171,17 +165,14 @@ namespace BackOffice.Seccion.Configuracion
                     }
                     else
                         mensajeLogin(true, mensajes.logWarningcamp, mensajes.tipoWarning);
-
                 }
                 else
                     mensajeLogin(true, mensajes.logWarningcamp, mensajes.tipoWarning);
-
             }
             else
                 mensajeLogin(true, mensajes.logErrcampvac, mensajes.tipoInfo);
             return false;
         }
-
 
         /// <summary>
         /// Metodo encargado de restablecer la contraseña 
@@ -205,6 +196,7 @@ namespace BackOffice.Seccion.Configuracion
              }
                  
          } 
+
         /// <summary>
         /// Metodo encargado de restablecer la nueva contraseña del usuario
         /// </summary>
@@ -216,8 +208,6 @@ namespace BackOffice.Seccion.Configuracion
              if (this.password1.Value != "")
              _employee.UserAccount.Password = this.password1.Value;
             
-        }
-
-                
+        }      
     }
 }
