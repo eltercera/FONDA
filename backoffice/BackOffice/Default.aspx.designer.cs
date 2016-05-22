cda9146fc3d7f0ba9f773ecf703ac1f8dc7d01f4
MalenaLi <malenarojasy@gmail.com>
2016-05-22 14:17:29 -0400

    numero de comensales

--- backoffice/BackOffice/Default.aspx.designer.cs
+++ backoffice/BackOffice/Default.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace BackOffice {
     public partial class Prueba {
         
         /// <summary>
+        /// commensalLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label commensalLabel;
+        
+        /// <summary>
         /// A1 control.
         /// </summary>
         /// <remarks>
