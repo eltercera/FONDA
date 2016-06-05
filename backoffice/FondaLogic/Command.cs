c99f413fdda3642cbe2ee3fa4316fb99d2de6fec
Marco Franceschi <marcofranceschi@hotmail.com>
2016-06-05 18:17:31 -0400

    Estructura para patrones

---
+++ backoffice/FondaLogic/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FondaLogic
+{
+    public abstract class Command<Input, Output>
+    {
+        public abstract Output Execute(Input param);
+    }
+}
