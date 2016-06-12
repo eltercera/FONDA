c49b9dc2c6b6f2c0910f633081e970e73671dff8
Rómulo Rodríguez <rodriguezrjrr@gmail.com>
2016-06-12 09:51:24 -0400

    Solve merge conflict

---
+++ backoffice/FondaBeckEndLogic/Exceptions/CreateFavoriteRestaurantCommandException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ds201625.fonda.FondaBackEndLogic.Exceptions
+{
+    public class CreateFavoriteRestaurantCommandException : FondaBackendLogicException
+    {
+        public CreateFavoriteRestaurantCommandException  () : base() {	}
+
+		public CreateFavoriteRestaurantCommandException (string message) : base(message) {	}
+
+        public CreateFavoriteRestaurantCommandException(string message, Exception InnerException)
+			: base(message, InnerException) {	}
+    }
+}
---
+++ backoffice/FondaBeckEndLogic/Exceptions/GetAllRestaurantsCommandException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ds201625.fonda.FondaBackEndLogic.Exceptions
+{
+    public class GetAllRestaurantsCommandException : FondaBackendLogicException
+    {
+        public GetAllRestaurantsCommandException () : base() {	}
+
+		public GetAllRestaurantsCommandException(string message) : base(message) {	}
+
+        public GetAllRestaurantsCommandException(string message, Exception InnerException)
+			: base(message, InnerException) {	}
+    }
+}
