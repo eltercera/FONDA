a968aea7b14fb4757fe5e51d2d9625c3498e11fc
MalenaLi <malenarojasy@gmail.com>
2016-06-12 15:32:07 -0400

    Dish Order Test

--- backoffice/FondaDataAccessTest/BODishOrderTest.cs
+++ backoffice/FondaDataAccessTest/BODishOrderTest.cs
@@ -4,6 +4,7 @@ using com.ds201625.fonda.DataAccess.FactoryDAO;
 using com.ds201625.fonda.DataAccess.InterfaceDAO;
 using com.ds201625.fonda.Domain;
 using System.Collections.Generic;
+using com.ds201625.fonda.Factory;
 
 namespace FondaDataAccessTest
 {
@@ -11,9 +12,11 @@ namespace FondaDataAccessTest
     [TestFixture()]
     public class BODishOrderTest
     {
-        private int accountId;
+        private int accountId, _number;
         private IList<DishOrder> _listDishOrder;
         private Account _account;
+        private Table _table;
+        private Commensal _commensal;
         private DishOrder _dishOrder;
         private FactoryDAO _facDAO;
         private IDishOrderDAO _dishOrderDAO;
@@ -23,12 +26,15 @@ namespace FondaDataAccessTest
         {
             if (_facDAO == null)
                 _facDAO = FactoryDAO.Intance;
-            
+
             _dishOrderDAO = _facDAO.GetDishOrderDAO();
+            _number = 0;
+            _table = new Table();
+            _commensal = new Commensal();
             _dishOrder = new DishOrder();
             _listDishOrder = new List<DishOrder>();
+            _account = (Account)EntityFactory.GetAccount(_table,_commensal,_listDishOrder,_number);
             accountId = 1;
-            _dishOrder = _dishOrderDAO.FindById(accountId);
 
 
         }
