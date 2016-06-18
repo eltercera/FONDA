fcb1c70a7da5b5dc29a06782f1ffd592e7238a99
Carlos Herrera <carlosdhm6@gmail.com>
2016-06-17 22:43:48 -0430

    Init manu MVP

---
+++ backoffice/BackOfficePresenter/Menu/MenuCategoriaPresenter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackOfficeModel.Menu;
+
+namespace com.ds201625.fonda.BackOffice.Presenter.Menu
+{
+    public class MenuCategoriaPresenter : BackOfficePresenter.Presenter
+    {
+        //Enlace Modelo - Vista
+        private IMenuCategory _view;
+        int totalColumns = 3;
+
+        ///<summary>
+        ///Constructor
+        /// </summary>
+        /// <param name="viewMenuCategory">Interfaz</param>
+        public MenuCategoriaPresenter(IMenuCategory page) : base(page)
+        {
+            _view = page;
+        }
+    }
+}
