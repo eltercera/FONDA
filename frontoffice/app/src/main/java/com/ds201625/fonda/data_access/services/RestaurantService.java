c60b66a414adf241e5d640b239778ffc517e1b3c
gbsojo <gbsojo@gmail.com>
2016-06-18 18:15:27 -0400

    Retrofit y Servicio para categories, zones y restaurants

---
+++ frontoffice/app/src/main/java/com/ds201625/fonda/data_access/services/RestaurantService.java
@@ -0,0 +1,19 @@
+package com.ds201625.fonda.data_access.services;
+
+import com.ds201625.fonda.domains.Restaurant;
+import com.ds201625.fonda.domains.RestaurantCategory;
+import com.ds201625.fonda.domains.Zone;
+
+import java.util.List;
+
+/**
+ * Interfaz para el servicio del restaurant
+ */
+public interface RestaurantService {
+
+    List<RestaurantCategory> getCategories(String query, int max, int page);
+
+    List<Zone> getZones(String query, int max, int page);
+
+    List<Restaurant> getRestaurants(String query, int max, int page, String category, String zone);
+}
