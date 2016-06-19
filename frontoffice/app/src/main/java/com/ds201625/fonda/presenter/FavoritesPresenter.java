2676eeac1bb67c286e8e06c13ef89c7ffaad5d67
melaniegodoy <melanie.g1257@gmail.com>
2016-06-18 20:31:28 -0430

    G6_Modficaciones

--- frontoffice/app/src/main/java/com/ds201625/fonda/presenter/FavoritesPresenter.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/presenter/FavoritesPresenter.java
@@ -1,5 +1,7 @@
 package com.ds201625.fonda.presenter;
 
+import android.util.Log;
+
 import com.ds201625.fonda.domains.Commensal;
 import com.ds201625.fonda.domains.Restaurant;
 import com.ds201625.fonda.interfaces.IFavoriteView;
@@ -21,6 +23,7 @@ public class FavoritesPresenter implements IFavoriteViewPresenter {
     private String emailToWebService;
     private FondaCommandFactory facCmd;
     private List<Restaurant> listRestWS;
+    private String TAG = "FavoritesPresenter";
 
     /**
      * Constructor
@@ -35,22 +38,27 @@ public class FavoritesPresenter implements IFavoriteViewPresenter {
      */
     @Override
     public void findLoggedComensal() {
+        Log.d(TAG,"Ha entrado en findLoggedComensal");
         Commensal log = SessionData.getInstance().getCommensal();
 
         emailToWebService=log.getEmail()+"/";
-
         facCmd = FondaCommandFactory.getInstance();
-
         //Llamo al comando de requireLogedCommensalCommand
         Command cmdRequireLoged = facCmd.requireLogedCommensalCommand();
         try {
             cmdRequireLoged.setParameter(0,emailToWebService);
             cmdRequireLoged.run();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (NullPointerException e){
+            Log.e(TAG,"Error en findLoggedComensal al buscar el comensal logueado",
+                    e);
+        }catch (Exception e) {
+            Log.e(TAG,"Error en findLoggedComensal al buscar el comensal logueado",
+                    e);
         }
 
         logedComensal = (Commensal) cmdRequireLoged.getResult();
+        Log.d(TAG,"Se obtiene el comensal logueado "+logedComensal.getId());
+        Log.d(TAG,"Ha finalizado findLoggedComensal");
     }
 
 
@@ -61,14 +69,22 @@ public class FavoritesPresenter implements IFavoriteViewPresenter {
      */
     @Override
     public List<Restaurant> findAllFavoriteRestaurant() {
+        Log.d(TAG,"Ha entrado en findAllFavoriteRestaurant");
         Command cmdAllFavorite = facCmd.allFavoriteRestaurantCommand();
         try {
             cmdAllFavorite.setParameter(0,logedComensal);
             cmdAllFavorite.run();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (NullPointerException e){
+            Log.e(TAG,"Error en findAllFavoriteRestaurant al buscar los restaurantes favoritos",
+                    e);
+        }
+        catch (Exception e) {
+            Log.e(TAG,"Error en findAllFavoriteRestaurant al buscar los restaurantes favoritos",
+                    e);
         }
         listRestWS = (List<Restaurant>) cmdAllFavorite.getResult();
+        Log.d(TAG,"Se retorna la lista de Restaurantes Favoritos");
+        Log.d(TAG,"Ha finalizado findAllFavoriteRestaurant");
         return listRestWS;
     }
 
@@ -79,14 +95,22 @@ public class FavoritesPresenter implements IFavoriteViewPresenter {
      */
     @Override
     public void deleteFavoriteRestaurant(Restaurant restaurant) {
+        Log.d(TAG,"Ha entrado en deleteFavoriteRestaurant");
         Command cmdDelete = facCmd.deleteFavoriteRestaurantCommand();
         try {
             cmdDelete.setParameter(0,logedComensal);
             cmdDelete.setParameter(1,restaurant);
             cmdDelete.run();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        catch (NullPointerException e){
+            Log.e(TAG,"Error en deleteFavoriteRestaurant al eliminar un restaurante favorito",
+                    e);
+        }catch (Exception e) {
+            Log.e(TAG,"Error en deleteFavoriteRestaurant al eliminar un restaurante favorito",
+                    e);
+        }
+        Log.d(TAG,"Se elimina el restaurante de favoritos "+restaurant.getName());
+        Log.d(TAG,"Ha finalizado deleteFavoriteRestaurant");
     }
 
     /**
@@ -96,16 +120,22 @@ public class FavoritesPresenter implements IFavoriteViewPresenter {
      */
     @Override
     public void addFavoriteRestaurant(Restaurant restaurant) {
+        Log.d(TAG,"Ha entrado en addFavoriteRestaurant");
         Command cmdAddFavorite = facCmd.addFavoriteRestaurantCommand();
         try {
             cmdAddFavorite.setParameter(0,logedComensal);
             cmdAddFavorite.setParameter(1,restaurant);
             cmdAddFavorite.run();
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (NullPointerException e){
+            Log.e(TAG,"Error en addFavoriteRestaurant al agregar un restaurante a favoritos",
+                    e);
+        }catch (Exception e) {
+            Log.e(TAG,"Error en addFavoriteRestaurant al agregar un restaurante a favoritos",
+                    e);
         }
-
+        Log.d(TAG,"Se agrega el restaurante a favoritos "+restaurant.getName());
+        Log.d(TAG,"Ha finalizado addFavoriteRestaurant");
     }
 
 
