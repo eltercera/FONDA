472455dc238f4ba0b1cce8d3fd59eb82a86be7d0
Marco Franceschi <marcofranceschi@hotmail.com>
2016-06-19 20:38:14 -0400

    Organizacion de archivos de pruebas unitarias

---
+++ backoffice/FondaBackOfficeLogicTest/OrderAccount/BOCommandInvoiceTest.cs
@@ -0,0 +1,115 @@
+﻿using com.ds201625.fonda;
+using com.ds201625.fonda.DataAccess.FactoryDAO;
+using com.ds201625.fonda.DataAccess.InterfaceDAO;
+using com.ds201625.fonda.Domain;
+using com.ds201625.fonda.Factory;
+using FondaLogic;
+using FondaLogic.Factory;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FondaBackOfficeLogicTest
+{
+    public class BOCommandInvoiceTest
+    {
+        private int _restaurantId, _profileId,_accountId, _invoiceId;
+        private Command _command;
+        private IList<Account> _listClosedOrders;
+        private IList<Invoice> _listInvoices;
+        private FactoryDAO _facDAO;
+        private IOrderAccountDao _orderAccountDAO;
+        private Restaurant _restaurant;
+        private Invoice _invoice;
+        private Account _account;
+        private IInvoiceDao _invoiceDAO;
+        private IList<object> _listObject;
+        private IList<int> _list;
+        private Profile _profile;
+        private CashPayment _cashPayment;
+        private CreditCardPayment _creditPayment;
+
+
+        [SetUp]
+        public void Init()
+        {
+            _facDAO = FactoryDAO.Intance;
+            _listObject = new List<object>();
+            _list = new List<int>();
+            _orderAccountDAO = _facDAO.GetOrderAccountDAO();
+            _restaurant = new Restaurant();
+            _listClosedOrders = new List<Account>();
+            _restaurant.Id = 1;
+            IRestaurantDAO _restaurantDAO = _facDAO.GetRestaurantDAO();
+            _restaurant = _restaurantDAO.FindById(_restaurant.Id);
+            _restaurantId = _profileId= 1;
+            _accountId = 3;
+            _invoiceId = 10;
+            _account = new Account();
+            _invoice = EntityFactory.GetInvoice();
+            _account.Id = 2;
+            _invoiceDAO = _facDAO.GetInvoiceDao();
+            IOrderAccountDao _accountDAO = _facDAO.GetOrderAccountDAO();
+            IProfileDAO _profileDao = _facDAO.GetProfileDAO();
+            IUserAccountDAO _uaDAO = _facDAO.GetUserAccountDAO();
+            _account = _accountDAO.FindById(_account.Id);
+            _listInvoices = new List<Invoice>();
+            _cashPayment = EntityFactory.GetCashPayment(100);
+            UserAccount ua = _uaDAO.FindById(20);
+            _profile =_profileDao.FindById(_profileId);
+            _invoice = _invoiceDAO.FindById(_invoiceId);
+            //IBaseEntityDAO<Payment> bla = _facDAO
+        }
+
+        [Test]
+        public void CommandFindInvoicesByRestaurantTest()
+        {
+
+            _command = CommandFactory.GetCommandFindInvoicesByRestaurant(_restaurant);
+
+            _command.Execute();
+
+            _listInvoices = (IList<Invoice>)_command.Receiver;
+
+            Assert.IsNotNull(_listInvoices);
+            Assert.AreEqual(_listInvoices[0].Id,1);
+            Assert.AreEqual(_listInvoices[1].Number, 2);
+        }
+
+        [Test]
+        public void CommandGenerateInvoiceTest()
+        {
+            InvoiceStatus i = _facDAO.GetGeneratedInvoiceStatus();
+            _invoice = EntityFactory.GetInvoice(_cashPayment, _profile, _cashPayment.Amount, ((_cashPayment.Amount)*0.12f), null, 100, i);
+           _listObject.Add(_invoice);
+            _listObject.Add(_restaurant.Id);
+            _listObject.Add(_accountId);
+            _command = CommandFactory.GetCommandGenerateInvoice(_listObject);
+
+            _command.Execute();
+           // _invoice = _invoiceDAO.FindById();
+        }
+
+        [Test]
+        public void CommandPrintInvoice()
+        {
+            InvoiceStatus i = _facDAO.GetGeneratedInvoiceStatus();
+            //_invoice = (Invoice)EntityFactory.GetInvoice(_cashPayment, _profile, 100, 4850, 0.12f, null, 100, i);
+            _list.Add(_accountId);
+            _list.Add(_restaurantId);
+            //_listEntity.Add(_account);
+            _command = CommandFactory.GetCommandPrintInvoice(_list);
+
+            _command.Execute();
+        }
+
+        //[TearDown]
+        //public void EndTests()
+        //{
+        //    _invoiceDAO.Delete(_invoice);
+        //}
+    }
+}
---
+++ backoffice/FondaDataAccessTest/OrderAccount/BOInvoiceTest.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+using System;
+using com.ds201625.fonda.DataAccess.InterfaceDAO;
+using com.ds201625.fonda.DataAccess.FactoryDAO;
+using com.ds201625.fonda.Domain;
+using System.Collections.Generic;
+using com.ds201625.fonda.Factory;
+
+namespace FondaDataAccessTest
+{
+    
+    
+    [TestFixture()]
+    public class BOInvoiceTests
+    {
+        #region Fields
+
+        private FactoryDAO _facDAO;
+        private IInvoiceDao _invoiceDAO;
+        private IOrderAccountDao _accountDAO;
+        private IProfileDAO _profileDao;
+        private IRestaurantDAO _restaurantDAO;
+        private Restaurant _restaurant;
+        private Invoice _invoice;
+        private Account _account;
+        private CashPayment _cashPayment;
+        private CreditCardPayment _creditPayment;
+        private Profile _profile;
+        private IList<Invoice> _listInvoices;
+        private int _number, _accountId, _restaurantId, _profileId;
+        private float _amount, _tax;
+
+        #endregion
+
+        [SetUp]
+        public void Init()
+        {
+            _facDAO = FactoryDAO.Intance;
+
+            //Llama a interfaces DAO
+            _restaurantDAO = _facDAO.GetRestaurantDAO();
+            _accountDAO = _facDAO.GetOrderAccountDAO();
+            _invoiceDAO = _facDAO.GetInvoiceDao();
+
+            //Inicializa variables
+            _accountId = 2;
+            _restaurantId = 1;
+            _profileId = 1;
+            _tax = _amount * 0.12F;
+
+            //Consigue eventos de la Base de Datos
+            _restaurant = _restaurantDAO.FindById(_restaurantId);
+            _account = _accountDAO.FindById(_accountId);
+            _profile = _profileDao.FindById(_profileId);
+
+            _number = _invoiceDAO.GenerateNumberInvoice(_restaurant);
+
+            //Instancia objetos a utilizar
+            _cashPayment = EntityFactory.GetCashPayment(_amount);
+            _invoice = EntityFactory.GetInvoice(
+                _cashPayment, _profile, _amount, _tax, _restaurant.Currency, _number);
+
+            
+            _listInvoices = new List<Invoice>();
+        }
+
+        [Test]
+        public void FindInvoiceByRestaurantTest()
+        {
+
+            _listInvoices = _invoiceDAO.FindInvoicesByRestaurant(_restaurant);
+            Assert.IsNotNull(_listInvoices);
+            Assert.AreEqual(_listInvoices[0].Id, 1);
+            Assert.AreEqual(_listInvoices[1].Id, 2);
+            Assert.AreEqual(_listInvoices[2].Number, 3);
+        }
+
+        [Test]
+        public void GenerateNumberInvoice()
+        {
+
+            _number = _invoiceDAO.GenerateNumberInvoice(_restaurant);
+            //Hay 6 facturas insertadas 
+            Assert.IsNotNull(_number);
+            Assert.AreEqual(_number,7);
+        }
+
+        [Test]
+        [Ignore("Probar los cambios realizados")]
+        public void FindGenerateInvoiceByAccountTest()
+        {
+
+            _invoice = _invoiceDAO.FindGenerateInvoiceByAccount(_account);
+            Assert.IsNotNull(_invoice);
+            Assert.AreEqual(_invoice.Id,2);
+            Assert.AreEqual(_invoice.Number, 2);
+        }
+
+        [Test]
+        [Ignore("Probar los cambios realizados")]
+        public void FindInvoicesByAccountTest()
+        {
+
+            _listInvoices = _invoiceDAO.FindInvoicesByAccount(_accountId);
+            Assert.IsNotNull(_listInvoices);
+            Assert.AreEqual(_listInvoices[0].Id,2);
+        }
+
+        [Test]
+        public void SaveInvoiceTest()
+        {
+            _invoiceDAO.Save(_invoice);
+
+        }
+
+
+        [TearDown]
+        public void EndTests()
+        {
+
+        }
+    }
+}
+
