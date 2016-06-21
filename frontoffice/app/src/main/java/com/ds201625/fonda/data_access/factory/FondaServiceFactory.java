2c813ca664623e05a17d11fa78a69edb6c1e81a4
gbsojo <gbsojo@gmail.com>
2016-06-21 15:55:11 -0400

    Creacion de comandos de Restaurant y Factory de los respectivos servicios

--- frontoffice/app/src/main/java/com/ds201625/fonda/data_access/factory/FondaServiceFactory.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/data_access/factory/FondaServiceFactory.java
@@ -14,6 +14,7 @@ import com.ds201625.fonda.data_access.services.PaymentService;
 import com.ds201625.fonda.data_access.services.ProfileService;
 import com.ds201625.fonda.data_access.services.RequireLogedCommensalService;
 import com.ds201625.fonda.data_access.services.ReservationService;
+import com.ds201625.fonda.data_access.services.RestaurantService;
 import com.ds201625.fonda.data_access.services.TokenService;
 import com.ds201625.fonda.data_access.services.ZoneService;
 import com.ds201625.fonda.domains.Commensal;
@@ -165,6 +166,10 @@ public class FondaServiceFactory {
      */
     public ReservationService getAllReservesService(){
         return serviceFactory.getAllReservesService();
+    }
+
+    public RestaurantService getRestaurantService() {
+        return serviceFactory.getRestaurantService();
     }
 
     /**
---
+++ frontoffice/app/src/main/java/com/ds201625/fonda/logic/Commands/ResturantCommands/getZonesCommand.java
@@ -0,0 +1,58 @@
+package com.ds201625.fonda.logic.Commands.ResturantCommands;
+
+import android.util.Log;
+
+import com.ds201625.fonda.data_access.factory.FondaServiceFactory;
+import com.ds201625.fonda.data_access.services.RestaurantService;
+import com.ds201625.fonda.domains.Zone;
+import com.ds201625.fonda.logic.BaseCommand;
+import com.ds201625.fonda.logic.Parameter;
+
+import java.util.List;
+
+/**
+ * Comando para obtener zonas
+ */
+public class getZonesCommand extends BaseCommand {
+
+    private String TAG = "GetZonesCommand";
+
+    /**
+     * Asignacion de los parametros del comando.
+     * Parametro en posicion 0: Query
+     * Parametro en posicion 1: Max
+     * Pareametro en posicion 2: Page
+     */
+    @Override
+    protected Parameter[] setParameters() {
+        Parameter [] parameters = new Parameter[1];
+        parameters[0] = new Parameter(String.class, true);
+        parameters[1] = new Parameter(int.class, true);
+        parameters[2] = new Parameter(int.class, true);
+
+        return parameters;
+    }
+
+    @Override
+    public void invoke () {
+        Log.d(TAG, "Comando para obtener la lista de Categorias");
+        List<Zone> zones = null;
+
+        RestaurantService resService = FondaServiceFactory.getInstance()
+                .getRestaurantService();
+
+        try {
+            String query = (String) getParameter(0);
+            int max = (int) getParameter(1);
+            int page = (int) getParameter(2);
+            zones = resService.getZones(query, max, page);
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Se ha generado error en invoke al obtener las zonas de restaurantes", e);
+            e.printStackTrace();
+        }
+
+        setResult(zones);
+    }
+
+}
