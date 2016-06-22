3f689236b1245b859607475720087edacb56f41e
gbsojo <gbsojo@gmail.com>
2016-06-21 22:25:12 -0400

    Correccion de cantidad de parametros en los command

--- frontoffice/app/src/main/java/com/ds201625/fonda/logic/Commands/ResturantCommands/getZonesCommand.java
+++ frontoffice/app/src/main/java/com/ds201625/fonda/logic/Commands/ResturantCommands/getZonesCommand.java
@@ -25,7 +25,7 @@ public class getZonesCommand extends BaseCommand {
      */
     @Override
     protected Parameter[] setParameters() {
-        Parameter [] parameters = new Parameter[1];
+        Parameter [] parameters = new Parameter[3];
         parameters[0] = new Parameter(String.class, true);
         parameters[1] = new Parameter(int.class, true);
         parameters[2] = new Parameter(int.class, true);
