6c7235140151b21306d87dd0d6c39bb1e9701f01
Marco Franceschi <marcofranceschi@hotmail.com>
2016-06-23 07:05:18 -0400

    Arreglo de errores por integracion

---
+++ backoffice/FondaBackEnd/Exceptions/GetPaymentHistoryByProfileException.cs
@@ -0,0 +1,18 @@
+﻿using com.ds201625.fonda.FondaBackEnd.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.ds201625.fonda.BackEnd.Exceptions
+{
+    public class GetPaymentHistoryByProfileException : FondaBackendException
+    {
+        public GetPaymentHistoryByProfileException() : base() { }
+
+        public GetPaymentHistoryByProfileException(string message) : base(message) { }
+
+        public GetPaymentHistoryByProfileException(string message, Exception InnerException)
+			: base(message, InnerException) { }
+    }
+}
